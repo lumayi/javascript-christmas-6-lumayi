f9afb72fb67134a6d841d63a717f27709cfcf038
greeneeya <suzythetravelerr@gmail.com>
2023-11-11 17:48:16 +0900

    feat(Order): 음료만 주문시, 예외처리한다.

--- src/Order.js
+++ src/Order.js
@@ -1,5 +1,4 @@
-import InputView from './InputView';
-import { ERROR_MESSAGE, ORDER } from './constants/constant';
+import { ERROR_MESSAGE } from './constants/constant';
 
 export default class Order {
   #menus;
@@ -11,15 +10,12 @@ export default class Order {
     this.#orders = [];
   }
 
-  async takeOrder() {
-    const order = await InputView.readOrder();
-  }
-
-  organizeOrder(order) {
+  takeOrder(order) {
     const organized = order.split(',');
     organized.forEach((item) => {
       this.#orders.push(this.#validate(item));
     });
+    this.#checkDrinkPolicy();
   }
 
   #validate(item) {
@@ -36,5 +32,12 @@ export default class Order {
     );
     if (existingMenu) throw new Error(ERROR_MESSAGE.INVALID_ORDER);
     return validated;
+  }
+
+  #checkDrinkPolicy() {
+    const drinks = this.#orders.filter((v) => v.section === 'drink');
+    if (this.#orders.length === drinks.length) {
+      throw new Error(ERROR_MESSAGE.NOT_ONLY_DRINKS);
+    }
   }
 }
--- src/constants/constant.js
+++ src/constants/constant.js
@@ -13,4 +13,5 @@ export const ORDER = Object.freeze({
 export const ERROR_MESSAGE = Object.freeze({
   INVALID_DATE: '[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요',
   INVALID_ORDER: '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
+  NOT_ONLY_DRINKS: '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
 });
