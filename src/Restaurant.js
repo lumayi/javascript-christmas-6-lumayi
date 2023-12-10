716201d1e317b548875ec67f42e30e5ef4a9fff8
greeneeya <suzythetravelerr@gmail.com>
2023-12-10 14:51:18 +0900

    feat: 사용자 입력, 콘솔 아웃풋 처리

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,3 +1,37 @@
-class Restaurant {
-  greetGuest() {}
+import WoowahanMenu from './Menu/Menu';
+import December from './Month/December';
+import Order from './Order/Order';
+import InputView from './Views/InputView';
+import OutputView from './Views/OutputView';
+
+export default class Restaurant {
+  order = new Order(WoowahanMenu);
+
+  async greetGuest() {
+    OutputView.printGreet();
+    const december = await this.#getVisitDate();
+    const placedOrder = await this.#getUserOrder(this.order);
+  }
+
+  async #getVisitDate() {
+    try {
+      const userDate = await InputView.readDate();
+      const december = new December(userDate);
+      return december;
+    } catch (error) {
+      OutputView.printError(error);
+      return this.#getVisitDate();
+    }
+  }
+
+  async #getUserOrder() {
+    try {
+      const userOrder = await InputView.getOrder();
+      const validatedOrders = this.order.placeOrder(userOrder);
+      return validatedOrders;
+    } catch (error) {
+      OutputView.printError(error);
+      return this.#getUserOrder();
+    }
+  }
 }
--- src/Views/OutputView.js
+++ src/Views/OutputView.js
@@ -1,8 +1,14 @@
+import { Console } from '@woowacourse/mission-utils';
+
 const OutputView = {
-  printMenu() {
-    Console.print('<주문 메뉴>');
-    // ...
+  printGreet() {
+    Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.');
   },
-  // ...
+  printBenefits() {
+    Console.print('12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n');
+  },
+  printError(error) {
+    Console.print(error.message);
+  },
 };
 export default OutputView;
